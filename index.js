594fc6efd930f82d55a3f560c2ad484aa82ed4bd
Afif-EC <56635900+Afif-EC@users.noreply.github.com>
2019-10-18 12:20:57 +0300

    chore(index.js): fix rules

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ module.exports = {
     'eslint-config-airbnb-base'
   ].map(require.resolve),
   rules: {
-    "indent": ["error", 4],
-    "func-names": "off",
-    "vars-on-top": "off",
-    "no-use-before-define": ["error", { "functions": false, "classes": true }],
-    "no-param-reassign": ["error", {"props":  false}],
-    "no-plusplus": ["error", { "allowForLoopAfterthoughts": true }],
-    "strict": ["error", "global"]
+    indent: ["error", 4],
+    func-names: "off",
+    vars-on-top: "off",
+    no-use-before-define: ["error", { "functions": false, "classes": true }],
+    no-param-reassign: ["error", {"props":  false}],
+    no-plusplus: ["error", { "allowForLoopAfterthoughts": true }],
+    strict: ["error", "global"]
   }
 };
