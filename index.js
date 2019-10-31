43e588e69e9551ab910e57d5c130953b621fba11
Afif El Charif <afif@coddict.com>
2019-10-31 09:29:07 +0000

    chore(index.js): remove ES6 rules

--- index.js
+++ index.js
@@ -9,16 +9,6 @@ module.exports = {
     "commonjs": false,
     "jquery": true
   },
-  "plugins": [
-    "dollar-sign",
-    "jquery",
-    "html"
-  ],
-  "settings": {
-    "html/html-extensions": [
-      ".html",
-      ".twig"]
-  },
   rules: {
     "indent": ["error", 4],
     "func-names": "off",
@@ -26,6 +16,37 @@ module.exports = {
     "no-use-before-define": ["error", { "functions": false, "classes": true }],
     "no-param-reassign": ["error", {"props":  false}],
     "no-plusplus": ["error", { "allowForLoopAfterthoughts": true }],
-    "strict": ["error", "global"]
+    "strict": ["error", "global"],
+    "arrow-body-style": "off",
+    "arrow-parens": "off",
+    "arrow-spacing": "off",
+    "constructor-super": "off",
+    "generator-star-spacing": "off",
+    "no-class-assign": "off",
+    "no-confusing-arrow": "off",
+    "no-const-assign": "off",
+    "no-dupe-class-members": "off",
+    "no-duplicate-imports": "error",
+    "no-new-symbol": "off",
+    "no-restricted-imports": "off",
+    "no-this-before-super": "off",
+    "no-useless-computed-key": "off",
+    "no-useless-constructor": "off",
+    "no-useless-rename": "off",
+    "no-var": "off",
+    "object-shorthand": "off",
+    "prefer-arrow-callback": "off",
+    "prefer-const": "off",
+    "prefer-destructuring": "off",
+    "prefer-numeric-literals": "off",
+    "prefer-reflect": "off",
+    "prefer-rest-params": "off",
+    "prefer-spread": "off",
+    "prefer-template": "off",
+    "require-yield": "off",
+    "rest-spread-spacing": "off",
+    "symbol-description": "off",
+    "template-curly-spacing": "off",
+    "yield-star-spacing": "off"
   }
 };
